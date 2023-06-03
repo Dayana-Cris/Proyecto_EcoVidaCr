da4beccaa24c97861800d1e60082b5969df165b0
Dayana-Cris <68671879+Dayana-Cris@users.noreply.github.com>
2023-06-02 18:52:12 -0600

    Commit Inicial del proyecto

---
+++ EcoVidaCR/EcoVidaCR/Controllers/VoluntariadosController.cs
@@ -0,0 +1,177 @@
+﻿using EcoVidaCR.Data;
+using EcoVidaCR.Models;
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace EcoVidaCR.Controllers
+{
+    public class VoluntariadosController : Controller
+    {
+        //Variable que permite manejar la referencia del contexto 
+        private readonly Contexto contexto;
+
+        public VoluntariadosController(Contexto context)
+        {
+            contexto = context;
+        }
+        //Es el primer metodo que se ejecuta en el controlador
+        public IActionResult Index()
+        {
+            return View(contexto.Voluntariados.ToList());
+        }
+
+        [HttpGet]
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("nombreVoluntariado,descripcion,idDestino,correo,telefono")] Voluntariados voluntariados)
+        {
+            if (ModelState.IsValid)
+            {
+                contexto.Add(voluntariados);
+                await contexto.SaveChangesAsync();
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(voluntariados);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Edit(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var voluntariado = await contexto.Voluntariados.FindAsync(Id);
+
+            if (voluntariado == null)
+            {
+                return NotFound();
+            }
+
+            return View(voluntariado);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int Id, [Bind("idVoluntariado,nombreVoluntariado,descripcion,idDestino,correo,telefono")] Voluntariados voluntariado)
+        {
+            if (Id != voluntariado.idVoluntariado)
+            {
+                return NotFound();
+            }
+            if (ModelState.IsValid)
+            {
+                contexto.Update(voluntariado);
+
+                await contexto.SaveChangesAsync();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return View(voluntariado);
+            }
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Delete(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var voluntariado = await contexto.Voluntariados.FindAsync(Id);
+
+            if (voluntariado == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return View(voluntariado);
+            }
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+
+        public async Task<IActionResult> Delete(int Id)
+        {
+            var voluntariado = await contexto.Voluntariados.FindAsync(Id);
+
+            contexto.Voluntariados.Remove(voluntariado);
+
+            await contexto.SaveChangesAsync();
+
+            return RedirectToAction("Index");
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> Details(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var voluntariado = await contexto.Voluntariados.FindAsync(Id);
+
+            if (voluntariado == null)
+            {
+                return NotFound();
+            }
+
+            return View(voluntariado);
+        }
+        [HttpGet]
+        public async Task<IActionResult> Enviado(int? Id)
+        {
+            if (Id == null)
+            {
+                return NotFound();
+            }
+
+            var voluntariado = await contexto.Voluntariados.FindAsync(Id);
+
+            EnviarEmail(voluntariado);
+
+            return View();
+        }
+        private bool EnviarEmail(Voluntariados voluntario)
+        {
+            try
+            {
+                bool enviado = false;
+                //gestiona la comunicación con el servidor de email
+                Email email = new Email();
+
+                var correo = User.Identity.Name;
+
+
+                //utilizamos el método pero le enviamos los datos del usuario dentro de la variable User
+                email.Enviar(correo, voluntario);
+
+                //si todo salio bien,  true representa envio exitoso.
+                enviado = true;
+
+                //enviamos la variable bandera
+                return enviado;
+            }
+            catch (Exception ex)
+            {
+
+                throw ex;
+            }
+        }
+    }
+}
---
+++ EcoVidaCR/EcoVidaCR/Models/Voluntariados.cs
@@ -0,0 +1,44 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+
+namespace EcoVidaCR.Models
+{
+    /*
+        idVoluntariado int not null primary key identity, 
+	    nombreVoluntariado varchar (500) not null,
+	    descripcion varchar (500) not null,
+	    nombreDestino varchar (500) not null,
+	    correo varchar (500) not null,
+	    telefono varchar (100)
+     */
+    public class Voluntariados
+    {
+        [Key]
+        public int idVoluntariado { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el nombre del voluntariado")]
+        [Display(Name = "Nombre del voluntariado")]
+        [StringLength(500, ErrorMessage = "El nombre que ingreso es demasiado largo")]
+        public string nombreVoluntariado { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar la descripcion del voluntariado")]
+        [Display(Name = "Descripcion del voluntariado")]
+        [StringLength(500, ErrorMessage = "La descripcion que ingreso es demasiada larga")]
+        public string descripcion { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el nombre del destino")]
+        [Display(Name = "Nombre del destino")]
+        public int idDestino { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el correo del usuario")]
+        [Display(Name = "Correo electronico")]
+        [StringLength(100, ErrorMessage = "El correo que ingreso es demasiado largo")]
+        [DataType(DataType.EmailAddress)]
+        public string correo { get; set; }
+
+        [Required(ErrorMessage = "Debe ingresar el telefono de contacto para el voluntariado")]
+        [Display(Name = "Telefono de contacto")]
+        [StringLength(15, ErrorMessage = "El telefono que agrego es demasiado largo")]
+        public string telefono { get; set; }
+    }
+}
